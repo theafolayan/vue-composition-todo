58d67fdd104a2818ab0021eb6695aa4f75537555
theafolayan <theafolayan@gmail.com>
2019-12-05 16:41:45 +0100

    Add todo and Remove Todo works perfectly

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <section class = "container">
+   <div class="card">
+     <div class="card-header"> <h3> Add New Todo</h3></div>
+
+   <div class="card-body">
+      <input type="text" class = "form-control" v-model ="state.todo"/>
+      <button class="btn btn-primary my-2" @click="addNewTodo"> Add Todo</button>
+   </div>
+   </div> 
+
+  <div class="container my-5">
+  <div class="card">
+    <div class="card-header"> <h3> MyTodos</h3></div>
+    <div class="card-body">
+     <ul class="list-group">
+       <li class="list-group-item" v-for="(todo, i) in state.todos" :key="i">
+        {{todo}} <i class="fa fa-trash" aria-hidden="true" @click="removeTodo(i)"></i>
+       </li>
+     </ul>
+    </div>
+  </div>
+  </div>
+ </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "@vue/composition-api";
+
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  setup(){
+    const {state, addNewTodo, removeTodo} = useTodo();
+    return {
+      state,
+      addNewTodo,
+      removeTodo
+    };
   }
+}
+function useTodo(){
+  let state = reactive({
+    todo: "",
+    todos: []
+  });
+  function addNewTodo(){
+    state.todos.push(state.todo);
+    state.todo = '';
+  }
+  function removeTodo(i){
+    state.todos.splice(i);
+  }
+  return{
+    state,
+    addNewTodo,
+    removeTodo
+  };
 }
 </script>
 
@@ -24,5 +68,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.fa {
+  color:red;
+  margin-left: 20px;
 }
 </style>
